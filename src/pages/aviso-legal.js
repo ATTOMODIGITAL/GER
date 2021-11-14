9473e4ed349bd42f75047d14d734d197412d7540
Ines de Palma <ipalmamasaveu@gmail.com>
2021-11-14 16:34:12 +0100

    hey

---
+++ src/pages/aviso-legal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Seo from "../components/seo"
+
+const AvisoLegal = () => {
+  return (
+    <>
+    <Seo
+      title="Aviso legal"
+      lang="es"
+      description="page description"
+      meta="metatilte"
+    />
+    <Layout>
+      <h1>Aviso Legal</h1>
+    </Layout>
+    </>
+  )
+}
+
+export default AvisoLegal
---
+++ src/pages/privacidad.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Seo from "../components/seo"
+
+const Privacidad = () => {
+  return (
+    <>
+    <Seo
+      title="Política de privacidad"
+      lang="es"
+      description="page description"
+      meta="metatilte"
+    />
+    <Layout>
+      <h1>privacidad</h1>
+    </Layout>
+    </>
+  )
+}
+
+export default Privacidad
