0602f1fdb288caed727604475f31f808246cd44c
Ines de Palma <ipalmamasaveu@gmail.com>
2021-11-14 19:20:33 +0100

    footerXL

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,26 +1,18 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
 
 import FooterXS from "./FooterXS"
 import FooterXL from "./FooterXL"
-import './Footer.scss'
+import "./Footer.scss"
 
 const Footer = () => {
   return (
     <>
       <div className="FooterXS">
-        <FooterXS
-        />
+        <FooterXS />
       </div>
 
       <div className="FooterXL">
-        {/* <FooterXL
-          email={data.email}
-          tlf={data.telefono}
-          dir1={data.direccionLinea1}
-          dir2={data.direccionLinea2}
-          ig={data.igUrl}
-        /> */}
+        <FooterXL />
       </div>
     </>
   )
--- src/components/Footer/FooterXL.js
+++ src/components/Footer/FooterXL.js
@@ -1,54 +1,34 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-import { IoLogoInstagram } from "react-icons/io"
 import { Link } from "gatsby"
 
-const FooterXL = ({ email, tlf, dir1, dir2, ig }) => {
+const FooterXL = () => {
   return (
     <>
-      <div className="FooterXL__upper">
-        <div className="FooterXL__upper--item">
-          <p>{dir1}</p>
-          <p>{dir2}</p>
-          <p>España</p>
-        </div>
-
-        <div className="FooterXL__upper--item">
-          <p>{tlf}</p>
-          <p>{email}</p>
-        </div>
-
-        <div className="FooterXL__upper--item">
-          <a href={ig} target="_blank" rel="noreferrer">
-            <IoLogoInstagram fontSize="2.3em" color="#47535B" />
-          </a>
+      <div className="FooterXL__bottom__attomo">
+        <p>Powered by</p>
+        <div className="FooterXL__bottom__attomo--img">
+          <StaticImage
+            src="../../images/attomo-black.png"
+            alt="Attomo"
+            placeholder="none"
+            style={{
+              zIndex: 1,
+            }}
+          />
         </div>
       </div>
 
-      <div className="FooterXL__bottom">
-        <div className="FooterXL__bottom__attomo">
-          <p>Powered by</p>
-          <div className="FooterXL__bottom__attomo--img"
-          >
-            <StaticImage
-              src="../../images/attomo-black.png"
-              alt="Attomo"
-              placeholder="none"
-              style={{
-                zIndex: 1,
-              }}
-            />
-          </div>
-        </div>
-
-        <div className="FooterXL__bottom__legal">
-          <Link to="/privacidad" className="FooterXL__bottom__legal--item">
-            <p style={{color: "#e3e7e1"}}>Política de privacidad</p>
-          </Link>
-          <Link to="/aviso-legal" className="FooterXL__bottom__legal--item">
-            <p style={{color: "#e3e7e1"}}>Aviso Legal</p>
-          </Link>
-        </div>
+      <div className="FooterXL__bottom__legal">
+        <Link to="/privacidad" className="FooterXL__bottom__legal--item">
+          <p style={{ color: "#e3e7e1" }}>Política de privacidad</p>
+        </Link>
+        <Link to="/cookies" className="FooterXL__bottom__legal--item">
+          <p style={{ color: "#e3e7e1" }}>Política de cookies</p>
+        </Link>
+        <Link to="/aviso-legal" className="FooterXL__bottom__legal--item">
+          <p style={{ color: "#e3e7e1" }}>Aviso Legal</p>
+        </Link>
       </div>
     </>
   )
