27ffbed619908b3e654946d14c18cbb81cad9209
Ines de Palma <ipalmamasaveu@gmail.com>
2021-12-05 00:11:27 +0100

    last

--- src/components/Forms/FormGroup.js
+++ src/components/Forms/FormGroup.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from "react"
+
+import useListRestQuery from "../../queries/useListRestQuery"
+import "./Form.scss"
+import Input from "./Input"
 
 const FormGroup = () => {
+  const data = useListRestQuery()
+
   return (
-    <div>
-      <p>Coming soon...</p>
-    </div>
+    <form method="post" className="Form">
+      <div className="Form__rests">
+        <label>
+        <span className="Form__rests--label">
+          Restaurante
+        </span>
+          <select>
+            <option value="Solo rests con grupos">Coming soon... </option>
+            <option value="Solo rests con grupos">Only restaurants with Group option</option>
+          </select>
+        </label>
+      </div>
+
+      <div className="Form__userInfo">
+        <Input name="name" type="text" text="Nombre" extraClass="infoInput" />
+        <Input name="email" type="email" text="Email" extraClass="infoInput" />
+        <Input name="phone" type="text" text="Teléfono" extraClass="infoInput" />
+      </div>
+
+      <div className="Form__coments">
+        <Input name="coments" type="text" text="Comentarios" extraClass="comentsInput" />
+      </div>
+
+      <div className="Form__checkboxes">
+        <label>
+          <span className="Form__checkboxes--checkLabel">
+            Acepto la política de privacidad y aviso legal
+          </span>
+          <input name="legal" type="checkbox" />
+        </label>
+        <label>
+          <span className="Form__checkboxes--checkLabel">
+            Acepto recibir comunicaciones comerciales
+          </span>
+          <input name="comms" type="checkbox" />
+        </label>
+      </div>
+
+      <div className="Form__button">
+        <button>Enviar</button>
+      </div>
+    </form>
   )
 }
 
--- src/pages/reservas-grupos.js
+++ src/pages/reservas-grupos.js
@@ -1,20 +1,25 @@
 import * as React from "react"
-import Layout from "../components/layout"
 
+import Layout from "../components/layout"
+import useWindowSize from "../hooks/useWindowSize"
 import Seo from "../components/seo"
 import FormGroup from "../components/Forms/FormGroup"
 import useViewport from "../hooks/useViewport"
+import GoBack from "../components/GoBack/GoBrack"
 
 const ReservasGrupo = () => {
+  const size = useWindowSize()
   useViewport()
+
   return (
     <Layout>
-      <Seo
-        title="Home"
-        lang="es"
-      />
+      <Seo title="Home" lang="es" />
+      {
+        size > 880 && (<GoBack />)
+      }
       <div className="Main__titles">
         <h1>Formulario para eventos</h1>
+        <h6>Reservas para Grupos</h6>
       </div>
       <div className="Main__element">
         <FormGroup />
