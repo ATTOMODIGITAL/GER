a93ecba315003893299cc39b1c880f75f67a2a93
Ines de Palma <ipalmamasaveu@gmail.com>
2021-12-13 20:49:18 +0100

    emails

--- src/components/BluredPic/BluredPic.js
+++ src/components/BluredPic/BluredPic.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby';
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-
 import "./BluredPic.scss"
 
 const BluredPic = ({img, class1, class2 }) => {
