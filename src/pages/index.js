efb2739209a74bb6bf56992ff092641ae88b8e43
Dayan Alvarez <dayan@attomo.digital>
2023-10-31 16:03:48 +0100

    fix number of restaurants

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,25 @@ import useHomesQuery from "../queries/useHomesQuery"
 //import useRichText from "../hooks/useRichText"
 import useListRestQuery from "../queries/useListRestQuery"
 import useViewport from "../hooks/useViewport"
+import { Script } from "gatsby"
 
 const IndexPage = () => {
   const data = useHomesQuery()
   const data2 = useListRestQuery()
+
+  function eliminarDuplicados(arr) {
+    const uniqueArray = []
+    for (const obj of arr) {
+      if (
+        !uniqueArray.some(item => JSON.stringify(item) === JSON.stringify(obj))
+      ) {
+        uniqueArray.push(obj)
+      }
+    }
+    return uniqueArray
+  }
+
+  const arraySinDuplicados = eliminarDuplicados(data2)
 
   const imgsXL = [
     data2[0].node.homeImg,
@@ -42,7 +57,18 @@ const IndexPage = () => {
         description={data.seoMetaDescription}
       />
       <Cookies />
-      <Home videoXS={data.videoMobile} videoDesktop={data.videoDesktop} restaurants={data2} imgsMob={imgsXL} imgsDesktop={imgsXL} links={urls} logos={logos} />
+      <Script>
+        {`function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"c7083958ef736a97d794353925bdd4b9"})});`}
+      </Script>
+      <Home
+        videoXS={data.videoMobile}
+        videoDesktop={data.videoDesktop}
+        restaurants={arraySinDuplicados}
+        imgsMob={imgsXL}
+        imgsDesktop={imgsXL}
+        links={urls}
+        logos={logos}
+      />
     </>
   )
 }
